4e486b4d82aabb6d5e1f1203e45c5e00f812c73c
Richard-Thompson <richardthompson1990@hotmail.co.uk>
2017-06-13 15:51:22 +0100

    adding search-subNav and styling

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {searchForRepo} from '../actions/actions';
 import SearchList from './SearchList';
 import Pagination from './Pagination';
+import '../css/SearchPage.css';
 
 
 class SearchPage extends Component {
@@ -16,12 +17,38 @@ class SearchPage extends Component {
   render () {
     return (
       <div>
+        <div className="search-subnav">
+            <div className="search-tab is-active">
+              Repositories
+            </div>
+            <div className="search-tab">
+              Code
+            </div>
+            <div className="search-tab">
+              Commits
+            </div>
+            <div className="search-tab">
+              Issues
+            </div>
+             <div className="search-tab">
+              Wikis
+            </div>
+            <div className="search-tab">
+              Users
+            </div>
+            <div className="adv-search">
+              Advance search
+            </div>
+          </div>
+
          <SearchList repos={this.props.search}/>
+
          <Pagination 
             pages={this.props.pages}
             activePage={this.props.activePage}
             searchTerm={this.props.params.searchTerm}
           />
+
       </div> 
     )
   }
